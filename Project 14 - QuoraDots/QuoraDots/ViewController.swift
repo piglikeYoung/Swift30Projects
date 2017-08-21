2d8304efd89d76e9b40e5ba20ed7b7835cecd672
pike young <316046510@qq.com>
2017-08-21 09:30:25 +0800

    ADD: 'Project 14 - QuoraDots'

---
+++ Project 14 - QuoraDots/QuoraDots/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  QuoraDots
+//
+//  Created by pike young on 2017/8/20.
+//  Copyright © 2017年 pike young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dotOne: UIImageView!
+    @IBOutlet weak var dotTwo: UIImageView!
+    @IBOutlet weak var dotThree: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startAnimation()
+    }
+
+    func startAnimation() {
+        dotOne.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        dotTwo.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        dotThree.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        
+        UIView.animate(withDuration: 0.6, delay: 0.0, options: [.repeat, .autoreverse], animations: { 
+            self.dotOne.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.6, delay: 0.2, options: [.repeat, .autoreverse], animations: {
+            self.dotTwo.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.6, delay: 0.4, options: [.repeat, .autoreverse], animations: {
+            self.dotThree.transform = CGAffineTransform.identity
+        }, completion: nil)
+    }
+
+
+}
+
